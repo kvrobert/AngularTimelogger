aee77a5694317c9ba0d3761eccde0acb000e344e
Robert Kiss-Vincze <32328361+kvrobert@users.noreply.github.com>
2018-01-22 12:43:30 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,8 @@ import { AppRoutingModule } from './/app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule        // RouteComponens hiányzik...azt is hozzá kell adni, hogy kezelni tudja a routokat.
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
